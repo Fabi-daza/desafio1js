66e6264910573d8b34ef05998539c124f03f1f49
Fabián Daza <fabian.daza2020@gmail.com>
2022-12-03 22:22:56 -0300

    Add EventListener and modify querySelector

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,30 @@
+let button = document.getElementById("btn")
+
+button.addEventListener("click", ingresarProducto)
+button.addEventListener("click", colorSelect)
+
 function ingresarProducto() {
-    let tituloProducto = document.querySelector("#name")
-    let cantidadProductos = document.querySelector("#amount").value
-    let precioProducto = document.querySelector("#price")
+    let tituloProducto = document.getElementById("name")
+    let cantidadProductos = document.getElementById("amount").value
+    let precioProducto = document.getElementById("price")
     let valorProducto = precioProducto.innerHTML
     let total = cantidadProductos * valorProducto
 
-    document.querySelector("#title").innerHTML = tituloProducto.innerHTML;
-    document.querySelector("#cantidad").innerHTML = cantidadProductos;
-    document.querySelector("#precio").innerHTML = "$" + total;
+    document.getElementById("title").innerHTML = tituloProducto.innerHTML;
+    document.getElementById("cantidad").innerHTML = cantidadProductos;
+    document.getElementById("precio").innerHTML = "$" + total;
 }
 
 function colorSelect() {
-    let selectColor = document.querySelector("#color-selector").value
+    let selectColor = document.getElementById("color-selector").value
     let colorPicker = document.querySelector(".color-picked")
     let imagenMostrada = document.querySelector(".img-product")
 
-    if (selectColor == "white"){
+    if (selectColor == "white") {
         colorPicker.style.backgroundColor = "#FFFFFF";
-        imagenMostrada.setAttribute("src","./assets/img/case-white1.png");
-    } else{
+        imagenMostrada.setAttribute("src", "./assets/img/case-white1.png");
+    } else {
         colorPicker.style.backgroundColor = "#FFC0CB";
-        imagenMostrada.setAttribute("src","./assets/img/case-pink1.png");
+        imagenMostrada.setAttribute("src", "./assets/img/case-pink1.png");
     }
 }
-
